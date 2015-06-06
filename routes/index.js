faa1bc092ac17c991a8cfb45a2f161e354a8f256
eu <eu@pceu.(none)>
2015-06-06 22:19:18 +0200

    a terceira

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ var quizController=require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Preguntas' });
+
 });
+
+router.get('/autor', function(req, res, next) {
+res.render('autor', { title: ' Yo ' });
+
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
