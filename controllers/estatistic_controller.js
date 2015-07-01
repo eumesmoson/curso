35fdc6faeb1229cbb52c263b59b09e19726bacef
eu <eu@pceu.(none)>
2015-07-01 20:56:22 +0200

    ultimo11

--- controllers/estatistic_controller.js
+++ controllers/estatistic_controller.js
@@ -5,21 +5,19 @@ var mens='';
 exports.datos=function(req,res){
 
 	
-	models.Quiz.count().then(function(count){
+	var a=models.Quiz.count().then(function(count){
 	
 		if(count===0){
         
         datos.push(count);
         mens='No hay datos :(';
 	}
-	else{datos.push(count);mens='adfsddfsd';}
+	else{datos.push(count);mens='';}
      
      });
-	models.Comment.count().then(function(count){
-
-    datos.push(count);});
+	var b=models.Comment.count().then(function(count){});
   
-
+ 
 	console.log('*****Array:'+datos.length+'----0----'+datos[0]+'----1---'+datos[1]+'*****')
      res.render('quizes/estadisticas', {title:'Estadísticas',
 		numpre:datos[0],numcom:datos[1],mensaje:mens});	
